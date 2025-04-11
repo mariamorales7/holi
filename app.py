f4d990465287057344702709cb8e864e06e13704
Sala 203 <sala203@ujaverianas.loc>
2025-04-11 14:53:51 -0500

    holi

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ tab1, tab2 = st.tabs(['Tab 1', 'Tab 2'])
 with tab1:
     fig, ax = plt.subplots(1, 3, figsize=(10, 4))
     tab_freq = ran['educ'].value_counts().sort_index()
-    ax[0].bar(tab_freq.index, tab_freq.values)
-    ax[1].hist(ran['edad'], bins=30)
-    ax[2].hist(ran['wage'], bins=40)
+    ax[0].bar(tab_freq.index, tab_freq.values, color = "pink")
+    ax[1].hist(ran['edad'], bins=30, color = "purple")
+    ax[2].hist(ran['wage'], bins=40, color = "blue")
     st.pyplot(fig)
 
     fig, ax = plt.subplots(1, 2, figsize= (10, 4))
-    ax[0].scatter(ran['educ'], ran['wage']) 
-    ax[1].scatter(ran['edad'], ran['wage'])
+    ax[0].scatter(ran['educ'], ran['wage'], color = "pink") 
+    ax[1].scatter(ran['edad'], ran['wage'], color = "purple")
     st.pyplot(fig)
 
 with tab2:
@@ -27,5 +27,7 @@ with tab2:
            path = [px.Constant("PGN"),
                  "Nombre Sector",
                  "Tipo de gasto"],
-           values ='Valor')
+           values ='Valor',
+           color='Valor', 
+           color_continuous_scale='Viridis')
     st.plotly_chart(fig)
